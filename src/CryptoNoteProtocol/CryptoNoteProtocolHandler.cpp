eee5f3827d5ed86faa215596dd13cfbf2e5a407a
PluraCoin <36419889+pluracoin@users.noreply.github.com>
2018-10-02 20:25:09 +0200

    New block notification to the default logging level

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -238,7 +238,7 @@ int CryptoNoteProtocolHandler::handleCommand(bool is_notify, int command, const 
 #undef HANDLE_NOTIFY
 
 int CryptoNoteProtocolHandler::handle_notify_new_block(int command, NOTIFY_NEW_BLOCK::request& arg, CryptoNoteConnectionContext& context) {
-  logger(Logging::TRACE) << context << "NOTIFY_NEW_BLOCK (hop " << arg.hop << ")";
+  logger(Logging::INFO) << context << "NOTIFY_NEW_BLOCK (hop " << arg.hop << ") Height: " << arg.current_blockchain_height;
 
   updateObservedHeight(arg.current_blockchain_height, context);
 
